e2ba23d3c05f122276ccd24726875c5b15dd0828
Tommy Li <toli@adobe.com>
2022-06-22 14:26:07 -0700

    dockerfile all-in mitmproxy

--- main2.py
+++ main2.py
@@ -3,5 +3,9 @@ print("wait.....")
 time.sleep(5)
 print("wait is over.")
 
+# proxies = {
+#   "https": "127.0.0.1:8080"
+# }
+# resp = requests.get(url="https://httpbin.org/anything/foobar1", verify=False, proxies=proxies)
 resp = requests.get(url="https://httpbin.org/anything/foobar1", verify=False)
 print(resp.text)
--- run2.sh
+++ run2.sh
@@ -1,5 +1,10 @@
 #/bin/sh
-
-mitmdump -w /home/mitmproxy/output.log &
+#
+mitmdump -w /home/mitmproxy/output.log --mode regular &
+# mitmdump --mode regular -s /code/har_dump.py --set hardump=/home/mitmproxy/output.har &
+export http_proxy="http://127.0.0.1:8080"
+export HTTP_PROXY="http://127.0.0.1:8080"
+export https_proxy="http://127.0.0.1:8080"
+export HTTPS_PROXY="http://127.0.0.1:8080"
 python main2.py
-cat /home/mitmproxy/output.log
+sudo cat /home/mitmproxy/output.log
